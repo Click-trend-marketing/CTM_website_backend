a02ab66aa1a726c2e8f8c0d0a7aae88fe441b738
unknown <vikas>
2024-11-06 14:26:13 +0530

    cores

--- app.js
+++ app.js
@@ -1,28 +1,3 @@
-// const express = require ('express');
-// const app = express();
-// require('dotenv').config()
-// const cors = require('cors');
-
-
-// app.use(express.json())
-// app.use(cors()); // Allow all origins or specify your frontend origin
-
-
-// const conn = require('./database/db');
-// const mongoose = require('mongoose');
-
-// mongoose.connect(conn.url).then(() => {
-//         console.log("Databse Connected Successfully!!");    
-//      }).catch(err => {
-//         console.log('Could not connect to the database', err);
-//          process.exit();
-//      });
-
-// module.exports = app
-
-
-
-// app.js
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -33,7 +8,7 @@ const app = express();
 
 // Allow CORS from both live and local environments
 const corsOptions = {
-  origin: ['http://62.72.30.215:8000', 'http://localhost:4000'], // Live and local origins
+  origin: ['http://62.72.30.215:8000', 'http://localhost:4000', 'http://localhost:3000'], // Live and local origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
 };
 
